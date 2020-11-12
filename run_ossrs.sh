80e0787bc45eb0f0faa9829ad647d14f1e65b773
chuongtran27 <chuongtran1999dn@gmail.com>
2020-11-12 11:07:26 +0700

    ok

---
+++ run_ossrs.sh
@@ -0,0 +1,7 @@
+FILE_NAME="hls.conf"
+if [ -n "$1" ];
+then
+	FILE_NAME=$1
+fi
+docker ps -a | awk '{ print $1,$2 }' | grep ossrs/srs:3 | awk '{print $1 }' | xargs -I {} docker rm -f {} | awk '{ print $1,$2 }' | grep ossrs/srs:3 | awk '{print $1 }' | xargs -I {} docker rm -f {}
+docker run -d -p 1935:1935 -p 1985:1985 -p 8080:8080 -v $PWD/$FILE_NAME:/usr/local/srs/conf/srs.conf ossrs/srs:3
