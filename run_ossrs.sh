4533c1c13713f72bc30d5410b360249ad0c410ce
Chuong Tran <chuongtran1999dn@gmail.com>
2021-02-13 08:33:33 +0700

    update main.conf

--- run_ossrs.sh
+++ run_ossrs.sh
@@ -3,5 +3,5 @@ if [ -n "$1" ];
 then
 	FILE_NAME=$1
 fi
-docker ps -a | awk '{ print $1,$2 }' | grep ossrs/srs:3 | awk '{print $1 }' | xargs -I {} docker rm -f {} | awk '{ print $1,$2 }' | grep ossrs/srs:3 | awk '{print $1 }' | xargs -I {} docker rm -f {}
-docker run -d --restart=always -p 1935:1935 -p 1985:1985 -p 8080:8080 -v $PWD/$FILE_NAME:/usr/local/srs/conf/srs.conf ossrs/srs:3
+docker ps -a | awk '{ print $1,$2 }' | grep ossrs/srs | awk '{print $1 }' | xargs -I {} docker rm -f {} | awk '{ print $1,$2 }' | grep ossrs/srs | awk '{print $1 }' | xargs -I {} docker rm -f {}
+docker run -d -p 1935:1935 -p 1985:1985 -p 8080:8080 -v $PWD/$FILE_NAME:/usr/local/srs/conf/srs.conf ossrs/srs
