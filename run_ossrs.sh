907d9866c6e9ed24b16d2ed65a2a2092797ab971
tridentOC <chuongtran1999dn@gmail.com>
2021-02-16 06:48:13 +0000

    ok

--- run_ossrs.sh
+++ run_ossrs.sh
@@ -4,4 +4,4 @@ then
 	FILE_NAME=$1
 fi
 docker ps -a | awk '{ print $1,$2 }' | grep ossrs/srs | awk '{print $1 }' | xargs -I {} docker rm -f {} | awk '{ print $1,$2 }' | grep ossrs/srs | awk '{print $1 }' | xargs -I {} docker rm -f {}
-docker run -d -p 1935:1935 -p 1985:1985 -p 8080:8080 -v $PWD/$FILE_NAME:/usr/local/srs/conf/srs.conf ossrs/srs
+docker run -d -p 1935:1935 -p 1985:1985 -p 8080:8080 -p 8085:8085 -v $PWD/$FILE_NAME:/usr/local/srs/conf/srs.conf ossrs/srs:3
